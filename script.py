e2cde045debc1f50e0daaf878d7385c14815be12
georgekun <jorj.knyazyan.15@gmail.com>
2023-08-23 11:44:26 +0300

    script for vk. V1

---
+++ script.py
@@ -0,0 +1,51 @@
+import vk_api
+from config import access_token,myID, words_for_search
+
+"""Получение ACCESS происходило не через основной вк апи, а через сторонний сервис.
+Поэтому есть смысл использовать не настойщий аккаунт"""
+class AccountVk():
+    last_id = -555555
+    def __init__(self):
+        self.session = vk_api.VkApi(token=access_token)
+        self.vk= self.session.get_api()
+
+    def get_latest_message(self,chat_id = 0):
+        """Метод возвращает последние сообщение чата. Индекс ["items"][0] указывает на самый первый чат в списке. 
+        поэтому стоит закрепить чат, который требуется слушать """
+        group = self.session.method(
+            "messages.getConversations",
+            {})
+        
+        id = group["items"][chat_id]["last_message"]["id"]
+        if id != self.last_id:
+          last_message = group["items"][chat_id]["last_message"]["text"]
+          self.last_id = id
+          return last_message
+          
+      
+    def filter(self,words:list,text:str):
+        #Проверка на наличие слов в тексте сообщения
+        for word in words:
+          if word in text:
+              return True
+          else:
+              continue
+
+    def send_message_to_id(self,user_id,text):
+        self.session.method("messages.send",{"user_id":user_id,"random_id":"0","message":text})
+
+
+def main():
+    account = AccountVk()
+    while True:
+        words = words_for_search
+        message = account.get_latest_message() 
+        if message:
+          wm = account.filter(words,message)
+          if wm:
+              for i in range(3):
+                  account.send_message_to_id(myID,"чекни беседу")
+
+  
+if __name__ == "__main__":
+    main()
